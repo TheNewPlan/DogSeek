c41f10c7d70a612fe697d478c754fe4b850ba61c
Matt Hallowell <matthewhallowell1@gmail.com>
2017-11-18 15:57:18 -0600

    Listview now feeds data properly.

--- app/src/main/java/com/example/matthallowell/dogseek/ListActivity.java
+++ app/src/main/java/com/example/matthallowell/dogseek/ListActivity.java
@@ -17,62 +17,57 @@ public class ListActivity extends android.app.ListActivity {
 
     private static final String TAG = "ListActivity";
     private ArrayList<Breed> tmp;
-    private Breed[] breeds;
+    private Breed[] filteredbreeds;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG,"onCreate");
+        Intent intent = getIntent();
+        tmp = (ArrayList<Breed>) intent.getSerializableExtra("Breeds");
+        filteredbreeds = tmp.toArray(new Breed[tmp.size()]);
+        for (Breed breed : filteredbreeds) {
+            Log.d(TAG, "Printing filtered breeds as toasts within ListActivity");
+            Toast.makeText(this, breed.toString(), Toast.LENGTH_SHORT).show();
+        }
 
         setListAdapter(new DogAdapter());
     }
 
-    @Override
-    protected void onStart(){
-        Log.d(TAG,"onStart");
-        super.onStart();
-        Intent intent = getIntent();
-        tmp = (ArrayList<Breed>) intent.getSerializableExtra("Breeds");
-        breeds = tmp.toArray(new Breed[tmp.size()]);
-        for (Breed breed : breeds) {
-            Log.d(TAG, "Printing breeds as toasts within ListActivity");
-            Toast.makeText(this, breed.toString(), Toast.LENGTH_SHORT).show();
-        }
-    }
 
     class DogAdapter extends BaseAdapter{
+
         private LayoutInflater inflater;
 
         @Override
-        public int getCount(){return 0;}
+        public int getCount(){return filteredbreeds.length;}
 
         @Override
-        public Object getItem(int i){return null;}
+        public Object getItem(int i){return filteredbreeds[i];}
 
         @Override
         public long getItemId(int i) {
-            return 0;
+            return i;
         }
 
         @Override
         public View getView(int i, View view, ViewGroup viewGroup) {
-//            View row = view;
-//            if (view == null){
-//                if (inflater == null){
-//                    inflater = (LayoutInflater) ListActivity.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-//                    row = inflater.inflate(R.layout.activity_details, viewGroup, false);
-//                }
-//            }
-//            //Setup resource images
-//            TextView name = row.findViewById(R.id.text1);
-//            TextView description = row.findViewById(R.id.text2);
-//
-//            Breed breed = breeds[i];
-//            name.setText(breed.getName());
-//            description.setText(breed.getShortDescription());
-//            //Setup resource images
-//            return row;
-            return null;
+            View row = view;
+            if (view == null){
+                if (inflater == null) {
+                    inflater = (LayoutInflater) ListActivity.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                }
+                row = inflater.inflate(R.layout.activity_details, viewGroup, false);
+            }
+            //Setup resource images
+            TextView name = row.findViewById(R.id.text1);
+            TextView description = row.findViewById(R.id.text2);
+
+            Breed breed = filteredbreeds[i];
+            name.setText(breed.getName());
+            description.setText(breed.getShortDescription());
+            //Setup resource images
+            return row;
         }
     }
 }
