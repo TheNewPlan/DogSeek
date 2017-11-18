a43f7f7df5d4bc77533f94873c0ecdb7e5eaddf6
Matt Hallowell <matthewhallowell1@gmail.com>
2017-11-18 15:44:27 -0600

    Converted arrayList back to Breed object array

--- app/src/main/java/com/example/matthallowell/dogseek/ListActivity.java
+++ app/src/main/java/com/example/matthallowell/dogseek/ListActivity.java
@@ -33,8 +33,8 @@ public class ListActivity extends android.app.ListActivity {
         super.onStart();
         Intent intent = getIntent();
         tmp = (ArrayList<Breed>) intent.getSerializableExtra("Breeds");
-        //breeds = tmp.toArray(new Breed[tmp.size()]);
-        for (Breed breed : tmp) {
+        breeds = tmp.toArray(new Breed[tmp.size()]);
+        for (Breed breed : breeds) {
             Log.d(TAG, "Printing breeds as toasts within ListActivity");
             Toast.makeText(this, breed.toString(), Toast.LENGTH_SHORT).show();
         }
