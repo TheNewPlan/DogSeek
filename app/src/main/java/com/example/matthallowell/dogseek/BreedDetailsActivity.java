d0ecf9af281ada3abbc375be1fcd32e55278a2e4
Matt Hallowell <matthewhallowell1@gmail.com>
2017-11-19 22:07:56 -0600

    Added method to parse uri's on the fly

--- app/src/main/java/com/example/matthallowell/dogseek/BreedDetailsActivity.java
+++ app/src/main/java/com/example/matthallowell/dogseek/BreedDetailsActivity.java
@@ -1,6 +1,7 @@
 package com.example.matthallowell.dogseek;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -16,7 +17,7 @@ public class BreedDetailsActivity extends AppCompatActivity {
 
     private static final String TAG = "DetailsActivity";
     private Swipe swipe;
-
+    private String uriName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,7 +61,10 @@ public class BreedDetailsActivity extends AppCompatActivity {
 
             @Override
             public void onSwipedDown(MotionEvent event) {
-
+                String url = "https://www.akc.org/dog-breeds/" + uriName;
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
             }
         });
         Log.d(TAG, "onCreate");
@@ -73,6 +77,8 @@ public class BreedDetailsActivity extends AppCompatActivity {
         // The activity is about to become visible.
         Intent intent = getIntent();
         if (intent != null) {
+            uriName = intent.getStringExtra("Name");
+            uriName.toLowerCase();
             TextView name = findViewById(R.id.text1);
             TextView description = findViewById(R.id.text2);
             ImageView icon = findViewById(R.id.image);
